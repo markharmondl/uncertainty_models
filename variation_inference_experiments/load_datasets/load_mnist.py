948ee30a96957f4810407a78e6d04bbcefc717c1
Mark Harmon <markharmon@pop-os.localdomain>
2019-11-06 20:28:13 -0500

    improvement to the load data function

--- variation_inference_experiments/load_datasets/load_mnist.py
+++ variation_inference_experiments/load_datasets/load_mnist.py
@@ -1,8 +1,7 @@
-from sklearn.model_selection import train_test_split
 import tensorflow as tf
 
 
-def load():
+def load_and_shuffle(batch_size, seed):
     mnist = tf.keras.datasets.mnist
 
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -12,9 +11,12 @@ def load():
     x_train = x_train[..., tf.newaxis]
     x_test = x_test[..., tf.newaxis]
 
-    return {'x_train': x_train,
-            'y_train': y_train,
-            'x_test': x_test,
-            'y_test': y_test}
+    train_data_tf = tf.data.Dataset.from_tensor_slices(
+        (x_train, y_train)).shuffle(seed).batch(batch_size)
+
+    test_data_tf = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(batch_size)
+
+    return {'train_data_tf': train_data_tf,
+            'test_data_tf': test_data_tf}
 
 
--- variation_inference_experiments/models/ffn.py
+++ variation_inference_experiments/models/ffn.py
@@ -3,7 +3,7 @@ from tensorflow.keras.layers import Input, Model
 import tensorflow_probability as tfp
 
 
-def create_flipout_ffn(hidden_layers, input_shape, output_shape, activation):
+def construct_flipout_feed_forward_network(hidden_layers, input_shape, output_shape, activation):
 
     inputs = Input(shape=input_shape)
     network_input = inputs
@@ -34,3 +34,42 @@ def create_flipout_ffn(hidden_layers, input_shape, output_shape, activation):
             'elbo_loss': elbo_loss,
             'probs': probs,
             'neg_log_liklihood': neg_log_likelihood}
+
+
+def construct_lrt_feed_forward_network(hidden_layers, input_shape, output_shape, activation):
+
+    inputs = Input(shape=input_shape)
+    network_input = inputs
+    for units in hidden_layers:
+        network_input = tfp.layers.DenseLocalReparameterization(units=units, activation=activation)(network_input)
+    outputs = tfp.layers.DenseLocalReparameterization(output_shape, activation=None)(network_input)
+
+    model = Model(inputs, outputs)
+
+    data_features = tf.placeholder(shape=[None, 28, 28, 1], dtype=tf.float32)
+    data_labels = tf.placeholder(shape=[None], dtype=tf.int32)
+    kl_penalty = tf.placeholder(shape=[], dtype=tf.float32)
+
+    logits = model(data_features)
+    probabilities = tf.nn.softmax(logits, axis=1)
+    labels_distribution = tfp.distributions.Categorical(logits=logits)
+    log_probs = labels_distribution.log_prob(data_labels)
+
+    negative_log_likelihood = -tf.reduce_mean(log_probs)
+    kl = sum(model.losses) / kl_penalty
+    elbo_loss = negative_log_likelihood + kl
+
+    correct_preds = tf.equal(tf.cast(data_labels, dtype=tf.int64), tf.argmax(probabilities, axis=1))
+    accuracy = tf.reduce_mean(tf.cast(correct_preds, tf.float32))
+
+    model_outputs = {'accuracy': accuracy,
+                    'elbo_loss': elbo_loss,
+                    'probabilities': probabilities,
+                    'negative_log_liklihood': negative_log_likelihood}
+
+    model_inputs = {'data_features': data_features,
+                    'data_labels': data_labels,
+                    'kl_penalty': kl_penalty}
+
+
+    return
